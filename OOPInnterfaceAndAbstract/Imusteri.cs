16e14294c14440cb35bc7b5d14f6e5adc7ed011e
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-11-30 12:30:31 +0300

    Interface Bölümü Eklendi

---
+++ OOPInnterfaceAndAbstract/Imusteri.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInnterfaceAndAbstract
+{
+    public interface Imusteri
+    {
+        //public ımusteri()
+        //{
+        // İnterFace içerisinde ctor kulanılmaz..
+        //}
+
+
+        //field
+        int Id { get; set; }
+        string isim {  get; set; }
+        string soyisim { get; set; }
+        //metot
+
+        int yeniKayit(string isim,string soyisim);
+        int kayıtDüzenle(int id,string isim,string soyisim);
+        int kayıtSil(int id);
+        //delegate
+        //vb..
+    }
+}
